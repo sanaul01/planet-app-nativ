eef2dfa10b4044ce7e02e10f3b51234440246934
sanaul01 <mdsanaulislam154@gmail.com>
2022-06-15 00:36:54 +0600

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { colors } from './src/components/data/screens/theme/colors';
+import { spacing } from './src/components/data/screens/theme/spacing';
+import { useFonts } from 'expo-font';
+import { typography } from './src/components/data/screens/theme/typography';
+import Text from './src/components/text/text';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/screen/Home';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require('./assets/fonts/Antonio-Medium.ttf'),
+    "Spartan-Bold": require('./assets/fonts/Spartan-Bold.ttf'),
+    "Spartan-Regular": require('./assets/fonts/Spartan-Regular.ttf'),
+  })
+
+  if(!loaded){
+    return(
+      <Text>Font is loading....</Text>
+    )
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator screenOptions = {{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.lightGreen,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/screen/Home.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, View, } from 'react-native'
+import React from 'react'
+import Text from '../text/text'
+
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
